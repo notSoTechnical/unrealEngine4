9af3c62bf93d9726a5ca3a09f20da2af49ca80de
notSoTechnical <50722184+notSoTechnical@users.noreply.github.com>
2020-03-08 22:10:11 -0400

    Add files via upload

---
+++ TripleX.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main()
+{
+    //std=standard; tells the diff. between names 
+    //:: Scope operator 
+    //cout = write to terminal 
+    std::cout << "Hello, World";
+    std::cout << std::endl;
+    std::cout << "Hello, World again";
+
+    int a = 4;
+
+    std::cout << a;
+
+    return 0; 
+}
